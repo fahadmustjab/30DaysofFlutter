8f7021ef8a5cd5b1204e2b8d976dadbe9a13f980
fahadmustjab <beingfahad786@gmail.com>
2022-11-09 10:19:26 +0500

    day3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:catalogue_app/home_page.dart';
+import 'package:catalogue_app/pages/home_page.dart';
+import 'package:catalogue_app/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,7 +10,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
     );
   }
 }
