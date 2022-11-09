906a3d4f85d4f639ca55c2d6ebc9556951455f02
fahadmustjab <beingfahad786@gmail.com>
2022-11-09 17:03:43 +0500

    day7 committed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,8 +1,16 @@
 import 'package:catalogue_app/utils/routes.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeButton = false;
 
   @override
   Widget build(BuildContext context) {
@@ -11,11 +19,16 @@ class LoginPage extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Image.asset("assets/images/login_image.png", fit: BoxFit.cover),
+              Image.asset(
+                "assets/images/hey.png",
+                fit: BoxFit.cover,
+                height: 200,
+                width: 200,
+              ),
               SizedBox(height: 20),
               Text(
-                "Welcome",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                "Welcome $name",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(
@@ -25,6 +38,10 @@ class LoginPage extends StatelessWidget {
                     TextFormField(
                       decoration: InputDecoration(
                           hintText: "Enter Username", labelText: "Username"),
+                      onChanged: (value) {
+                        name = value;
+                        setState(() {});
+                      },
                     ),
                     TextFormField(
                       obscureText: true,
@@ -32,12 +49,47 @@ class LoginPage extends StatelessWidget {
                           hintText: "Enter Password", labelText: "Password"),
                     ),
                     SizedBox(height: 20),
-                    ElevatedButton(
-                        child: Text("Login"),
-                        style: TextButton.styleFrom(minimumSize: Size(100, 50)),
-                        onPressed: () {
+                    Material(
+                      color: Colors.deepPurple,
+                      borderRadius:
+                          BorderRadius.circular(changeButton ? 50 : 8),
+                      child: InkWell(
+                        onTap: () async {
+                          setState(() {
+                            changeButton = true;
+                          });
+                          await Future.delayed(Duration(seconds: 1));
                           Navigator.pushNamed(context, MyRoutes.HomeRoute);
-                        })
+                          setState(() {
+                            changeButton = false;
+                          });
+                        },
+                        child: AnimatedContainer(
+                          duration: Duration(seconds: 1),
+                          width: changeButton ? 50 : 150,
+                          height: 50,
+                          alignment: Alignment.center,
+                          child: changeButton
+                              ? Icon(
+                                  Icons.done,
+                                  color: Colors.white,
+                                )
+                              : Text(
+                                  "Login",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18),
+                                ),
+                        ),
+                      ),
+                    )
+                    // ElevatedButton(
+                    //     child: Text("Login"),
+                    //     style: TextButton.styleFrom(minimumSize: Size(100, 50)),
+                    //     onPressed: () {
+                    //       Navigator.pushNamed(context, MyRoutes.HomeRoute);
+                    //     })
                   ],
                 ),
               )
